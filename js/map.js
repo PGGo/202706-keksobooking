030b60ec838d05d24fc8d13c0d7fdf30d7a7df6c
PGGo <pggoncharova@gmail.com>
2017-04-09 22:15:14 +0300

    Исправила недочеты, добавила функцию отрисовки элементов. Есть ошибки

--- js/map.js
+++ js/map.js
@@ -17,50 +17,81 @@ var randomInteger = function (min, max) {
 //-----------------------
 
 var pinMap = document.querySelector('.tokyo__pin-map'); //сам тег
-var pin = pinMap.querySelector('.pin').content;
+var pin = pinMap.querySelector('.pin');
 var similarLodgeTemplate = document.querySelector('#lodge-template').content;
-var dialogPanel = document.querySelector('.dialog__panel').content; //content?
+var dialogPanel = document.querySelector('.dialog__panel');
 
 
 //-----------
 
-var lodges = [];
+var translateLodgeType = function (type) {
+  var typeOfLodge = type;
+  switch (typeOfLodge) {
+    case 'flat':
+      typeOfLodge = 'Квартира';
+      break;
+    case 'house':
+      typeOfLodge = 'Дом';
+      break;
+    case 'bungalo':
+      typeOfLodge = 'Бунгало';
+      break;
+  }
 
-for (var i = 0; i < 8; i++) {
-  var lodgeItem = {
-    author: {
-      avatar: 'img/avatars/user0' + (i + 1) + 'png'
-    },
+  return typeOfLodge;
+};
 
-    offer: {
-      title: TITLE[i],
-      address: lodgeItem.location.x + ', ' + lodgeItem.location.y,
-      price: randomInteger(1000, 1000000),
-      type: TYPE[randomInteger(0, 2)], //здесь случайное значение?
-      rooms: randomInteger(1, 5),
-      guests: randomInteger(1, 15), //а здесь какой максимум?
-      checkin: CHECKINOUT[randomInteger(0, 2)],
-      checkout: CHECKINOUT[randomInteger(0, 2)],
-      features: function () {
-        var randomFeatures;
-        for (i = 0; i <= randomInteger(0, 5); i++) {
-          randomFeatures[i] = FEATURES[i];
-        }
+var getLodgeFeatures = function (features) {
+  var lodgeFeatures = '';
+  for (var i = 0; i <= features.length; i++) {
+    lodgeFeatures = lodgeFeatures + '<span class=feature__image feature__image--' + features[i] + '>' + features[i] + '</span>';
+  }
 
-        return randomFeatures;
+  return lodgeFeatures;
+};
+
+var generateLodges = function (lodgesNumber) {
+  var lodges = [];
+
+  for (var i = 0; i < lodgesNumber; i++) {
+    var lodgeItem = {
+      author: {
+        avatar: 'img/avatars/user0' + (i + 1) + 'png'
       },
-      description: '',
-      photos: []
-    },
 
-    location: {
-      x: randomInteger(300, 900),
-      y: randomInteger(100, 500)
-    }
-  };
+      offer: {
+        title: TITLE[i],
+        address: lodgeItem.location.x + ', ' + lodgeItem.location.y, //не могу понять что не так
+        price: randomInteger(1000, 1000000),
+        type: TYPE[randomInteger(0, 2)], //здесь случайное значение?
+        rooms: randomInteger(1, 5),
+        guests: randomInteger(1, 15), //а здесь какой максимум?
+        checkin: CHECKINOUT[randomInteger(0, 2)],
+        checkout: CHECKINOUT[randomInteger(0, 2)],
+        features: function () { //проблема с этой функцией!!
+          var randomFeatures;
+          for (var j = 0; j <= randomInteger(0, 5); j++) {
+            randomFeatures[j] = FEATURES[j];
+          }
 
-  lodges[i] = lodgeItem;
-}
+          return randomFeatures;
+        },
+        description: '',
+        photos: []
+      },
+
+      location: {
+        x: randomInteger(300, 900),
+        y: randomInteger(100, 500)
+      }
+    };
+
+    lodges[i] = lodgeItem;
+  }
+
+  return lodges;
+};
+
 
 var createPinMapElement = function (lodge) {
   var pinMapElement = pin.cloneNode(true);
@@ -68,10 +99,9 @@ var createPinMapElement = function (lodge) {
   pinMapElement.setAttribute('style', 'left: ' + lodge.location.x + ';');
   pinMapElement.setAttribute('style', 'top: ' + lodge.location.y + ';');
 
-  //pinMapElement.querySelector('.rounded').setAttribute('src', lodge.author.avatar);
-  //или     можно и так и так?
+  pinMapElement.querySelector('.rounded').setAttribute('src', lodge.author.avatar);
   //координаты метки не учитывала
-  pinMapElement.querySelector('.rounded').src = lodge.author.avatar;
+
 
   return pinMapElement;
   /* <div class="pin" style="left: {{location.x}}px; top: {{location.y}}px">
@@ -85,9 +115,9 @@ var createLodgeElement = function (lodge) {
   lodgeElement.querySelector('.lodge__title').textContent = lodge.offer.title;
   lodgeElement.querySelector('.lodge__address').textContent = lodge.offer.address;
   lodgeElement.querySelector('.lodge__price').textContent = lodge.offer.price + '&#x20bd;/ночь';
-  lodgeElement.querySelector('.lodge__type').textContent = ''; //нужна дополнительная функция, которая будет выводить значение в соответствии с типом?
+  lodgeElement.querySelector('.lodge__type').textContent = translateLodgeType(lodge.offer.type);
   lodgeElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + lodge.offer.checkin + ' выезд до ' + lodge.offer.checkout;
-  lodgeElement.querySelector('.lodge__features').textContent = ''; //в процессе, скорее всего innerHTML
+  lodgeElement.querySelector('.lodge__features').innerHTML = getLodgeFeatures(lodge.offer.features);
   lodgeElement.querySelector('.lodge__description').textContent = lodge.offer.description;
 
   return lodgeElement;
@@ -102,8 +132,27 @@ var createLodgeElement = function (lodge) {
 Замените src у аватарки пользователя — изображения, которое записано в .dialog__title — на значения поля author.avatar отрисовываемого объекта. */
 };
 
-var fragment = document.createDocumentFragment();
+//----
 
-for (var j = 0; j < lodges.length; j++) {
-  fragment.appendChild(createLodgeElement(lodges[j]));
-}
+var renderPinMap = function (pinMapArr) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < pinMapArr.length; i++) {
+    fragment.appendChild(createPinMapElement(pinMapArr[i]))
+  }
+
+  pinMap.appendChild(fragment);
+};
+
+var renderLodges = function (lodgeArr) {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < lodgeArr.length; i++) {
+    fragment.appendChild(createLodgeElement(lodgeArr[i]))
+  }
+
+  dialogPanel.appendChild(fragment);
+};
+
+renderPinMap(generateLodges(8));
+renderLodges(generateLodges(8));
